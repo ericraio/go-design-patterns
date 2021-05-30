bef8f0e03e8a99189d5a06a0c6893549a22cdb38
Eric Raio <eraio@ezoic.com>
2021-05-30 10:36:19 -0700

    Adding abstract factory

---
+++ abstract-factory/iSportsFactory.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// iSportsFactory abstract factory interface.
+type iSportsFactory interface {
+	makeShoe() iShoe
+	makeShirt() iShirt
+}
+
+func getSportsFactory(brand string) (iSportsFactory, error) {
+	if brand == "adidas" {
+		return &adidas{}, nil
+	}
+
+	if brand == "nike" {
+		return &nike{}, nil
+	}
+
+	return nil, fmt.Errorf("Wrong brand type passed")
+}
